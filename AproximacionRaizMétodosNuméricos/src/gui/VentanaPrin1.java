2a973dc9e0b7ef1410018e391baa6b88bf157d72
Francisco <Francisco@192.168.1.68>
2019-08-22 09:11:08 -0500

    newton raphson

---
+++ AproximacionRaizMétodosNuméricos/src/gui/VentanaPrin1.java
@@ -0,0 +1,21 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VentanaPrin1 extends JFrame{
+	private JLabel lblPuntoI;
+	private JLabel lblError;
+	private JButton btnCalcular;
+	private JTextField txtPuntoI;
+	private JTextField txtError;
+//	private PnlInsercion pnlInsDtos;
+//	private PnlTabla tabla;
+
+	public VentanaPrin1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
