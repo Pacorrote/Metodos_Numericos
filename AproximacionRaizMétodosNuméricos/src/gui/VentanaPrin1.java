879da3a812e579f2dc0e1b73265dc21ef888907a
Francisco <Francisco@192.168.1.68>
2019-08-22 11:20:54 -0500

    Prueba ventana

--- AproximacionRaizMétodosNuméricos/src/gui/VentanaPrin1.java
+++ AproximacionRaizMétodosNuméricos/src/gui/VentanaPrin1.java
@@ -1,21 +1,25 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class VentanaPrin1 extends JFrame{
-	private JLabel lblPuntoI;
-	private JLabel lblError;
-	private JButton btnCalcular;
-	private JTextField txtPuntoI;
-	private JTextField txtError;
-//	private PnlInsercion pnlInsDtos;
-//	private PnlTabla tabla;
+
 
 	public VentanaPrin1() {
 		// TODO Auto-generated constructor stub
+		super.setSize(550, 600);
+		super.setBackground(Color.orange);
+		super.setLayout(new BorderLayout(8, 1));
+		super.setLocationRelativeTo(null);
+		
+		super.setVisible(true);
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
--- AproximacionRaizMétodosNuméricos/src/main/Main.java
+++ AproximacionRaizMétodosNuméricos/src/main/Main.java
@@ -1,16 +1,17 @@
 package main;
 
-import gui.VentanaPrin;
+import gui.VentanaPrin1;
 import objetos.Metodo_Newton_Raphson;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//new VentanaPrin();
+		new VentanaPrin1();
 		Metodo_Newton_Raphson metodo = new Metodo_Newton_Raphson(0f, 0.01f);
 		metodo.Metodo();
 		metodo.imprimir();
+		
 	}
 
 }
