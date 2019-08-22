978459514997011c3c2ca323bc02d13bdc67dba1
Francisco <Francisco@192.168.1.68>
2019-08-22 10:34:15 -0500

    Prueba 2

--- AproximacionRaizMétodosNuméricos/src/main/Main.java
+++ AproximacionRaizMétodosNuméricos/src/main/Main.java
@@ -1,13 +1,16 @@
 package main;
 
 import gui.VentanaPrin;
+import objetos.Metodo_Newton_Raphson;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new VentanaPrin();
-		
+		//new VentanaPrin();
+		Metodo_Newton_Raphson metodo = new Metodo_Newton_Raphson(0f, 0.01f);
+		metodo.Metodo();
+		metodo.imprimir();
 	}
 
 }
--- AproximacionRaizMétodosNuméricos/src/objetos/Metodo_Newton_Raphson.java
+++ AproximacionRaizMétodosNuméricos/src/objetos/Metodo_Newton_Raphson.java
@@ -7,14 +7,17 @@ public class Metodo_Newton_Raphson {
 	private ArrayList<ColumnasNewtonRaphson> filas;
 	private Float puntoI;
 	private ColumnasNewtonRaphson aux;
+	private Float error;
 
 	public Metodo_Newton_Raphson() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Metodo_Newton_Raphson(Float puntoI) {
-		 aux = new ColumnasNewtonRaphson(puntoI);
-		 this.puntoI = aux.getXi();
+	public Metodo_Newton_Raphson(Float puntoI, Float error) {
+		this.filas = new ArrayList<>();
+		this.error = error;
+		aux = new ColumnasNewtonRaphson(puntoI);
+		this.puntoI = aux.getXi();
 	}
 	
 	
@@ -23,7 +26,7 @@ public class Metodo_Newton_Raphson {
 		aux.setDiffxi(derivadaFuncion());
 		aux.setXj(Xj());
 		filas.add(aux);
-		if(this.puntoI!=aux.getXj()) {
+		if(error()>error) {
 			float aux = this.aux.getXj();
 			this.aux = new ColumnasNewtonRaphson(aux);
 			this.puntoI = this.aux.getXi();
@@ -31,15 +34,15 @@ public class Metodo_Newton_Raphson {
 		}
 	}
 	
-	public Float funcion() {
-		return (float) (Math.exp(this.puntoI)-this.puntoI);
+	private Float funcion() {
+		return (float) (Math.exp(-this.puntoI)-this.puntoI);
 	}
 	
-	public Float derivadaFuncion() {
-		return (float) (-Math.exp(this.puntoI)-1);
+	private Float derivadaFuncion() {
+		return (float) (-Math.exp(-this.puntoI)-1);
 	}
 	
-	public Float Xj() {
+	private Float Xj() {
 		if(aux.getDiffxi()!=0) {
 			return (this.puntoI-(aux.getFxi()/aux.getDiffxi()));
 		}
@@ -49,10 +52,14 @@ public class Metodo_Newton_Raphson {
 	}
 	
 	public void imprimir() {
-		System.out.println("xi\tf(xi)\tdiff(xi)\txi+1");
+		System.out.println("   xi\t   f(xi)\t   diff(xi)\t   xi+1");
 		for (ColumnasNewtonRaphson columnasNewtonRaphson : filas) {
-			columnasNewtonRaphson.toString();
+			System.out.println(columnasNewtonRaphson.toString());
 		}
+	}
+	
+	private Float error() {
+		return Math.abs(this.puntoI-aux.getXj())/aux.getXj();
 	}
 
 }
